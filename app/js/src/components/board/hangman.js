da01b22a7186f5e47b104b4bec8ded39eddf03a8
Marcin <walas.marcin@gmail.com>
2016-02-18 00:07:35 +0100

    hangman is done

--- app/js/src/components/board/hangman.js
+++ app/js/src/components/board/hangman.js
@@ -2,13 +2,21 @@
 
 import React, { PropTypes } from 'react';
 
+
+const STATE_MAP = ['bar', 'head', 'neck', 'corpus', 'right-arm', 'left-arm', 'right-hand', 'left-hand', 'right-leg', 'left-leg', 'right-foot', 'left-foot'];
+
 class Hangman extends React.Component{
-   
+
     render() {
         let className = `hangmanState${this.props.state }`;
+        let bodyElements = [];
+        for(let i = 0; i < this.props.state; i++){
+            bodyElements.push(<span key={i} className={ 'hangman-' + STATE_MAP[i] }></span>);        
+        }
+                
         return (           
             <div className="hangman-body">
-                <p>hangman state: { this.props.state }</p>
+                {bodyElements}
             </div>
         );
     }
